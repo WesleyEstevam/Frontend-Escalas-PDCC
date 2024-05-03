1e4f9cb8c06dd0e017202c33d1ae5b10604f4cb3
WesleyEstevam <wesleyestevam15@gmail.com>
2024-05-02 22:10:20 -0300

    rota de cadastro excluida

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import CadastroEscala from "./pages/CadastroEscala";
 import "./index.css";
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cadastro-escala" element={<CadastroEscala />} />
       </Routes>
     </BrowserRouter>
   );
